22b1b4e08cbd1a9c5ad5c91b931ec4d3210ccf79
Alang Kim <rlaangh77@gmail.com>
2025-01-19 19:52:24 +0900

    fix: 노출된 서비스롤 키 제거
    - 로컬 개발 환경에서 생성된 키이고, 실제 상용 환경의 키는 아니기 때문에 큰 문제는 없음.

--- supabase/functions/create-missions/index.ts
+++ supabase/functions/create-missions/index.ts
@@ -103,7 +103,7 @@ Deno.serve(async (req) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/create-missions' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU' \
+    --header 'Authorization: Bearer {serviceRoleKey}' \
     --header 'Content-Type: application/json' \
 
 */
